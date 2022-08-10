94ca0f09674ad7a09394c85d977028850a9507d2
GabrieleLerani <92364167+GabrieleLerani@users.noreply.github.com>
2022-08-10 08:54:25 +0200

    Delete final-example-airport directory

--- final-example-airport/utils/validation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-extern bool init_validation(void);
-extern void fini_validation(void);
-extern bool validate_time(char *str);
-extern bool validate_date(char *str);
