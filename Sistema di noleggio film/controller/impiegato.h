5a31563fb59918a321d67afe88ae131d54684b8d
GabrieleLerani <92364167+GabrieleLerani@users.noreply.github.com>
2022-09-14 15:58:02 +0200

    Delete Sistema di noleggio film directory

--- Sistema di noleggio film/controller/impiegato.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-extern void impiegato_controller(void);
--- Sistema di noleggio film/view/login.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "../model/db.h"
-
-extern bool ask_for_relogin(void);
-extern void view_login(struct credentials *cred);
